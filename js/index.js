0cc5e0550eba4eed390cbc1612aabf756e99b430
tarou652 <rrrr20050307@gmail.com>
2023-09-11 15:45:42 +0900

    新しいデザインの完成

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function(){
     $("#header").each(function(){
-        var $BurgerIcon=$(this).find("i");
-        var $BurgerMenu=$(this).find(".burger");
+        var $BurgerIcon=$(this).find("#sumaho_i");
+        var $BurgerMenu=$(this).find("#sumaho_burger");
         $BurgerIcon.on("click",function(){
             if($BurgerMenu.hasClass("active")){
                 $BurgerMenu.removeClass("active");
@@ -11,6 +11,20 @@ $(function(){
                 $BurgerMenu.addClass("active");
                 $BurgerIcon.removeClass("bi-list");
                 $BurgerIcon.addClass("bi-x");
+            }
+        })
+        var $pc_BurgerIcon=$(this).find("#pc_i");
+        var $pc_BurgerMenu=$(this).find("#pc_burger");
+        console.log($pc_BurgerMenu)
+        $pc_BurgerIcon.on("click",function(){
+            if($pc_BurgerMenu.hasClass("active")){
+                $pc_BurgerMenu.removeClass("active");
+                $pc_BurgerIcon.removeClass("bi-x");
+                $pc_BurgerIcon.addClass("bi-list");
+            }else{
+                $pc_BurgerMenu.addClass("active");
+                $pc_BurgerIcon.removeClass("bi-list");
+                $pc_BurgerIcon.addClass("bi-x");
             }
         })
     })
