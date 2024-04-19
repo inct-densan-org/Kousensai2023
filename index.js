1bf769d127eeb9fe2c5697b2ba1722e5614300f6
tarou652 <rrrr20050307@gmail.com>
2024-04-19 10:02:19 +0900

    なぜか前のバージョンに戻っていたのを修整

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ $(function(){
     $("#header").each(function(){
         var $BurgerIcon=$(this).find("#sumaho_i");
         var $BurgerMenu=$(this).find("#sumaho_burger");
-        
-        
         $BurgerIcon.on("click",function(){
             if($BurgerMenu.hasClass("active")){
                 $BurgerMenu.removeClass("active");
@@ -21,7 +19,7 @@ $(function(){
         })
         var $pc_BurgerIcon=$(this).find("#pc_i");
         var $pc_BurgerMenu=$(this).find("#pc_burger");
-        console.log($pc_BurgerMenu)
+        
         $pc_BurgerIcon.on("click",function(){
             if($pc_BurgerMenu.hasClass("active")){
                 $pc_BurgerMenu.removeClass("active");
@@ -32,15 +30,26 @@ $(function(){
                 $pc_BurgerIcon.removeClass("bi-list");
                 $pc_BurgerIcon.addClass("bi-x");
             }
-            $(window).resize(function(){
-                $pc_BurgerMenu.removeClass("active");
-                    $pc_BurgerIcon.removeClass("bi-x");
-                    $pc_BurgerIcon.addClass("bi-list");
-              });
         })
+        $(window).resize(function(){
+            $pc_BurgerMenu.removeClass("active");
+                $pc_BurgerIcon.removeClass("bi-x");
+                $pc_BurgerIcon.addClass("bi-list");
+          });
     })
 })
+function burgurchange(target){
+    let targetid="#"+target;
+    let windowhight=window.innerHeight;
+    let scrolltop=$(window).scrollTop();
+    
+    if ( scrolltop<= windowhight ) {
+        $(targetid).removeClass("active2");
+    } else{
+        $(targetid).addClass("active2");
+    }
 
+}
 function fadein(target,ticking){
     let elem = document.getElementById(target);
     let targetid="#"+target;
@@ -55,39 +64,122 @@ function fadein(target,ticking){
     }
 
 }
-function burgurchange(target){
-    let targetid="#"+target;
-    let windowhight=window.innerHeight;
-    let scrolltop=$(window).scrollTop();
-    
-    if ( scrolltop<= windowhight ) {
-        $(targetid).removeClass("active2");
-    } else{
-        $(targetid).addClass("active2");
-    }
-
-}
 let Isre2=false,Iskv=false,Islist=false,Ismap=false,Ispanhu=false;
 Iskv= fadein("kv",!Iskv);
+
 document.addEventListener("scroll", (event) => {
-    burgurchange("sumaho_i");
   Isre2= fadein("re2",!Isre2);
+  burgurchange("sumaho_i");
   Iskv= fadein("kv",!Iskv);
   Islist= fadein("planlist",!Islist);
   Ismap= fadein("planmap",!Ismap);
   Ispanhu= fadein("panhuimg",!Ispanhu);
-  
 
 });
 // ボタン要素を取得
 let nextPageButton = document.getElementById("nextPageButton");
-console.log(nextPageButton);
+
 // ボタンがクリックされたときの処理を定義
 nextPageButton.addEventListener("click", () => {
   // クエリパラメータを含むURLを生成
-  const nextPageURL = "../plan.html?source=button";
+  const nextPageURL = "../plan.html?source=button"
   
   // 生成したURLに移動
   window.location.href = nextPageURL;
 });
+ // スムーズスクロールの関数を定義
+function smoothScroll(targetId) {
+    const targetElement = document.getElementById(targetId);
 
+    if (targetElement) {
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
+
+// ページ内リンクがクリックされたときにスムーズスクロールを実行
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        smoothScroll(targetId);
+    });
+});
+// 企画リストのコンポーネント化
+class info extends HTMLElement { 
+    constructor() {
+      super();
+    }
+    connectedCallback(){
+        // カスタム要素の中にシャドウDOMを接続する
+      const shadow = this.attachShadow({ mode: 'open' });
+      
+      // HTMLファイルで指定された属性を取り出し、初期表示に使用する
+      const day = this.getAttribute('day') || 'unknown';
+      const genre = this.getAttribute('genre') || 'unknown';
+      const text = this.getAttribute('text') || 'unknown';
+  
+      // (1) このカスタム要素の文書構造を定義する
+      shadow.innerHTML = `
+      <style>
+      .info{
+        display:flex ;
+        justify-content: center;
+        margin-top: 20px;
+    }
+    .info-genre{
+        background-color: var(--sub-color);
+        width: 125px;
+        text-align: center;
+        color: white;
+        height: 36px;
+    
+    }
+    
+    .info-text{
+        width: 480px;
+        margin-left: 5%;
+    }
+    .day{
+        width: 180px;
+    }
+    @media screen and (max-width: 992px){
+        .info{
+            display:flex ;
+            justify-content:flex-start;
+            flex-wrap: wrap;
+            margin-top: 20px;
+        }
+        .info-genre{
+            background-color: var(--sub-color);
+            width: 90px;
+            text-align: center;
+            color: white;
+            height: 24px;
+            font-size: 16px;
+            display:flex ;
+            align-items: center;
+            justify-content: center;
+        }
+        
+        .info-text{
+            width: 100%;
+            margin-left: 0;
+            font-size: 20px;
+        }
+        .day{
+            width: 130px;
+            font-size: 16px;
+        }
+        }
+    }
+      </style>
+      <div class="info">
+        <div class="day">${day}</div>
+        <div class="info-genre">${genre}</div>
+        <div class="info-text">${text}</div>
+    </div>
+      `;
+}}customElements.define("info-element", info); 
--- plan.js
+++ plan.js
@@ -1,35 +1,116 @@
-$(function(){
-    $("#change_screen").each(function(){
-        var $tabList =$(this).find(".tabs-nav"),
-            $tavAnchors=$tabList.find("a"),
-            $tabpannel=$(this).find(".pannel");
-        $tabList.on("click","a",function(event){
-            event.preventDefault();
-            var $this=$(this);
-            if($this.hasClass("active")){
-                return;
-            }
-            $tavAnchors.removeClass("active");
-            $tavAnchors.closest(".NAV").removeClass("active");
-            $this.addClass("active");
-            $this.closest(".NAV").addClass("active");
-            $tabpannel.hide();
-            $($this.attr("href")).show();
-        })
-        $tavAnchors.eq(0).trigger("click");
+   // スムーズスクロールの関数を定義
+   const targetElement = document.getElementById("yatai");
+   
+function smoothScroll(targetId) {
+    const targetElement = document.getElementById(targetId);
+    
+    if (targetElement) {
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
+
+// ページ内リンクがクリックされたときにスムーズスクロールを実行
+document.querySelectorAll('area[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
         
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        
+        smoothScroll(targetId);
+    });
+});
+$('img[usemap]').rwdImageMaps();
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        
+        var firstChar = targetId.charAt(0); // 最初の文字を取得
+        if(firstChar=="n"){
+            showmap();
+            smoothScroll(targetId);
+        }else if(firstChar=="m"){
+            showlist();
+            smoothScroll(targetId);
+        }
+        
+    });
+});
+function showlist(){
+    
+    $("#list-button").addClass("active");
+    $("#list-button").closest(".NAV").addClass("active");
+    $("#map-button").removeClass("active");
+    $("#map-button").closest(".NAV").removeClass("active");
+    $("#map").hide();
+    $("#list").show();
+    const allOlElements = document.querySelectorAll("ol");
+
+    
+}
+function showmap(){
+    $("#map-button").addClass("active");
+    $("#map-button").closest(".NAV").addClass("active");
+    $("#list-button").removeClass("active");
+    $("#list-button").closest(".NAV").removeClass("active");
+    $("#list").hide();
+    $("#map").show();
+    const allOlElements = document.querySelectorAll("ol");
+
+    
+}
+function burgurchange(target){
+    let targetid="#"+target;
+    let scrolltop=$(window).scrollTop();
+    if ( scrolltop<= 100) {
+        $(targetid).removeClass("active2");
+    } else{
+        $(targetid).addClass("active2");
+    }
+
+}
+document.addEventListener("scroll", (event) => {
+    burgurchange("sumaho_i");
+  
+  });
+$(function(){
+    $("#map-button").on("click",function(){
+        
+        showmap()
     })
+    $("#list-button").on("click",function(){
+        const allOlElements = document.querySelectorAll("ol");
+        // すべての<ol>要素に対して処理を行うループ
+        allOlElements.forEach((ol) => {
+        // <ol>要素の下にあるすべての<li>要素を取得
+        const liElements = ol.querySelectorAll("li");
+    
+        // すべての<li>要素からactiveクラスを削除
+            liElements.forEach((li) => {
+                li.classList.remove("active");
+                });
+            });
+        showlist()
+    })
+        
     var $BurgerIcon=$(this).find("#sumaho_i");
         var $BurgerMenu=$(this).find("#sumaho_burger");
         $BurgerIcon.on("click",function(){
             if($BurgerMenu.hasClass("active")){
                 $BurgerMenu.removeClass("active");
+                $BurgerIcon.removeClass("active");
                 $BurgerIcon.removeClass("bi-x");
                 $BurgerIcon.addClass("bi-list");
+                $('body').css('overflow', 'auto');
             }else{
                 $BurgerMenu.addClass("active");
+                $BurgerIcon.addClass("active");
                 $BurgerIcon.removeClass("bi-list");
                 $BurgerIcon.addClass("bi-x");
+                $('body').css('overflow', 'hidden');
             }
         })
 })
@@ -43,15 +124,223 @@ window.addEventListener("load", () => {
     
     // "source"パラメータが "button" の場合、ボタンからアクセスされたことを示す処理を追加
     if (source === "button") {
+        showlist()
+      // ここにボタンからアクセスされた場合の特別な処理を追加
+    } else {
+        showmap()
+      
+      // 通常のリンクからアクセスされた場合の処理を追加
+    }
+  });
+
+
+// 企画リストのコンポーネント化
+  class plan_list extends HTMLElement { 
+    constructor() {
+      super();
+    }
+    connectedCallback(){
+        // カスタム要素の中にシャドウDOMを接続する
+      const shadow = this.attachShadow({ mode: 'open' });
+      
+      // HTMLファイルで指定された属性を取り出し、初期表示に使用する
+      const imgurl = this.getAttribute('imgurl') || 'unknown';
+      const genre = this.getAttribute('genre') || 'unknown';
+      const headline = this.getAttribute('headline') || 'unknown';
+      const text = this.getAttribute('text') || 'unknown';
+      const place = this.getAttribute('place') || 'unknown';
+      const href = this.getAttribute('href') || 'unknown';
+  
+      // (1) このカスタム要素の文書構造を定義する
+      shadow.innerHTML = `
+      <style>
+      .list_content{
+        width: 350px;
+        margin: 60px 15px 0;
+        height: 560px;
+        position: relative;
+    }
+    .list_content h1{
+        color: var(--footer-color-deep);
+        font-size: 1.6rem;
+        margin: 0;
+        font-weight: bold;
+    }
+    .list_img{
+        border-radius: 10px;
+        width: 100%;
+        height: 0;
+        padding: 62.69% 0 0;
+        background-position: 50% 50%;
+        background-size: contain;
+        background-repeat: no-repeat;
+        margin-bottom: 20px;
+        max-height: 280px;
+    }
+    .list_genre{
+        background-color: var(--footer-color-deep);
+        color: white;
+        text-align: center;
+        padding: 1% 3%;
+        font-size: large;
+        margin-right: 10px;
+        display: inline-block;
+    }
+    .list_content h3{
+        margin-top: 2%;
+        font-size: larger;
+        font-weight: normal;
+        line-height: 1.2;
+        color: var(--para-color);
+        margin-bottom: 6px;
+    }
+    .list_content p{
+        color: var(--acsent-color);
+        font-size: 1.1rem;
+        margin:0;
+    }
+    .list_button{
+        position: absolute;
+        display: inline-block;
+        padding: 15px 30px;
+        background-color: white;
+        border: var(--footer-color-deep) 2px solid;
+        color: var(--footer-color-deep);
+        text-align: center;
+        border-radius: 60px;
+        margin: auto;
+        margin-top: auto;
+        max-width: 340px;
+        font-weight: 700;
+        bottom: 0;
+        left: 70px;
+    }
+    a{
+        text-decoration: none;
+    }
+    @media screen and (max-width: 768px){
+        .list_content{
+                width: 350px;
+                margin:20px auto;
+            }
+        }
+    }
+      </style>
+      <div class="list_content" >
+        <div  style="background-image: url(${imgurl});" class="list_img"></div>
+            <div class="d-flex">
+                <div class="list_genre">${genre}</div>
+            </div>
+            <h1>${headline}</h1>
+            
+            <h3>${text}</h3>
+            <p>${place}</p>
+            <div style="width: 100%; text-align: center;"><a href="${href}" class="list_button" id="button">マップで確認　→</a></div>
+        </div>
+     </div>
+      `;
+      const myElement = shadow.getElementById('button');
+        myElement.addEventListener('click', function(e)  {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            
+            const targetLi = document.getElementById(targetId);
+            // すべての<ol>要素を取得
+            const allOlElements = document.querySelectorAll("ol");
+
+            // すべての<ol>要素に対して処理を行うループ
+            allOlElements.forEach((ol) => {
+            // <ol>要素の下にあるすべての<li>要素を取得
+            const liElements = ol.querySelectorAll("li");
+
+            // すべての<li>要素からactiveクラスを削除
+                liElements.forEach((li) => {
+                    li.classList.remove("active");
+                });
+            });
+            // 最初に取得した<li>要素にactiveクラスを追加
+            targetLi.classList.add("active");
+            var substring = targetId.substring(1);
+            var subint=parseInt(substring)
+            var firstChar = targetId.charAt(0); // 最初の文字を取得
+            
+            if(firstChar=="n"){
+                showmap();
+                if(subint<=15||subint==41||subint==42){
+                    smoothScroll("outside");
+                }else if(20<=subint&&subint<23||subint==29){
+                    smoothScroll("senkouka1")
+                }else if(subint==25||subint==292||subint==30){
+                    smoothScroll("senkouka2")
+                }else if(34==subint||subint==36||subint==35){
+                    smoothScroll("senkouka3")
+                }else if(17==subint||subint==18||subint==28||subint==33||subint==39){
+                    smoothScroll("kyouikuka1")
+                }else if(subint==16||subint==19||subint==27||subint==32||subint==37||subint==38){
+                    smoothScroll("kyouikuka2")
+                }else if(23==subint||subint==24||subint==26||subint==31){
+                    smoothScroll("kyouikuka3")
+                }else if(40==subint||subint==44||subint==43){
+                    smoothScroll("taiiku1")
+                }
+            }else if(firstChar=="m"){
+                showlist();
+                smoothScroll(targetId);
+            }
+        })  ;
+    function showlist(){
         $("#list-button").addClass("active");
         $("#list-button").closest(".NAV").addClass("active");
         $("#map-button").removeClass("active");
         $("#map-button").closest(".NAV").removeClass("active");
         $("#map").hide();
         $("#list").show();
-      // ここにボタンからアクセスされた場合の特別な処理を追加
-    } else {
-      console.log("通常のリンクからアクセスされました。");
-      // 通常のリンクからアクセスされた場合の処理を追加
     }
-  });
+    function showmap(){
+        $("#map-button").addClass("active");
+        $("#map-button").closest(".NAV").addClass("active");
+        $("#list-button").removeClass("active");
+        $("#list-button").closest(".NAV").removeClass("active");
+        $("#list").hide();
+        $("#map").show();
+    }
+    function Onactive(targetid) {
+        targetid.addClass("active")
+      }
+      function Offactive(targetid) {
+        targetid.removeClass("active")
+      }
+    function smoothScroll(targetId) {
+        const targetElement = document.getElementById(targetId);
+        
+        
+        if(window.innerWidth<900){
+            if (targetId=="outside") {
+                window.scrollTo({
+                    top: targetElement.offsetTop+targetElement.offsetHeight/2,
+                    behavior: 'smooth'
+                });}else{
+                    window.scrollTo({
+                        top: targetElement.offsetTop,
+                        behavior: 'smooth'
+                    });
+                }
+        }else{
+            if (targetId=="outside") {
+                window.scrollTo({
+                    top: targetElement.offsetTop+targetElement.offsetHeight,
+                    behavior: 'smooth'
+                });
+            }else{
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        }
+        
+    }
+    }
+      
+}
+  customElements.define("plan-list", plan_list); 
